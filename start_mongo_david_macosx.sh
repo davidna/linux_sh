0a3dd8abe0b1f7f09e48321e5d418705590996cc
David Na <dna@davidna.com>
2013-05-01 23:45:15 -0700

    mongo start on David's MacOSX

---
+++ start_mongo_david_macosx.sh
@@ -0,0 +1,24 @@
+Wed May  1 23:44:47 [initandlisten] MongoDB starting : pid=89896 port=27017 dbpath=/Users/dna/DB/mongodb-osx-x86_64 64-bit host=dna-mbp.att.net
+Wed May  1 23:44:47 [initandlisten] 
+Wed May  1 23:44:47 [initandlisten] ** WARNING: soft rlimits too low. Number of files is 256, should be at least 1000
+Wed May  1 23:44:47 [initandlisten] db version v2.2.0, pdfile version 4.5
+Wed May  1 23:44:47 [initandlisten] git version: f5e83eae9cfbec7fb7a071321928f00d1b0c5207
+Wed May  1 23:44:47 [initandlisten] build info: Darwin bs-osx-106-x86-64-1.local 10.8.0 Darwin Kernel Version 10.8.0: Tue Jun  7 16:33:36 PDT 2011; root:xnu-1504.15.3~1/RELEASE_I386 i386 BOOST_LIB_VERSION=1_49
+Wed May  1 23:44:47 [initandlisten] options: { dbpath: "/Users/dna/DB/mongodb-osx-x86_64" }
+Wed May  1 23:44:47 [initandlisten] exception in initAndListen: 10296 
+*********************************************************************
+ ERROR: dbpath (/Users/dna/DB/mongodb-osx-x86_64) does not exist.
+ Create this directory or give existing directory in --dbpath.
+ See http://dochub.mongodb.org/core/startingandstoppingmongo
+*********************************************************************
+, terminating
+Wed May  1 23:44:47 dbexit: 
+Wed May  1 23:44:47 [initandlisten] shutdown: going to close listening sockets...
+Wed May  1 23:44:47 [initandlisten] shutdown: going to flush diaglog...
+Wed May  1 23:44:47 [initandlisten] shutdown: going to close sockets...
+Wed May  1 23:44:47 [initandlisten] shutdown: waiting for fs preallocator...
+Wed May  1 23:44:47 [initandlisten] shutdown: lock for final commit...
+Wed May  1 23:44:47 [initandlisten] shutdown: final commit...
+Wed May  1 23:44:47 [initandlisten] shutdown: closing all files...
+Wed May  1 23:44:47 [initandlisten] closeAllFiles() finished
+Wed May  1 23:44:47 dbexit: really exiting now
